99ad175fc198644cbbae605a1f598c34713aba87
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-23 11:18:53 +0530

    Add files via upload

---
+++ src/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState:{
+        name: "",
+        email: ""
+    },
+    reducers: {
+        updateName: (state, action) => {
+            state.name = action.payload;
+        },
+        updateEmail: (state, action) => {
+            state.email = action.payload;
+        }
+    }
+})
+
+export const { updateName, updateEmail } = userSlice.actions;
+
+export default userSlice.reducer;
