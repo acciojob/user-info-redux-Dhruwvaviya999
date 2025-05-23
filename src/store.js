8ea2554a9cbc1c5a68ccde8f93f8000a1028b221
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-23 12:18:44 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./state/userSlice";
+import userReducer from "./userSlice";
 
 export default configureStore({
     reducer: {
